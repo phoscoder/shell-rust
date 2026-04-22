57d73f463ef28c998d96b9dd5b6356d0f87023e3
Victor (Takunda) Phos <victorphos1@gmail.com>
2026-04-23 01:45:43 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::path::Path;
+use std::os::unix::fs::PermissionsExt;
+use std::fmt::Display;
+
+
+fn is_executable(path: &str) -> bool {
+    match std::fs::metadata(path) {
+        Ok(metadata) => {
+            let mode = metadata.permissions().mode();
+            mode & 0o111 != 0
+        },
+        Err(_) => false
+    }
+}
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
+    
+    let path = std::env::var("PATH").unwrap();
     
     loop {
         print!("$ ");
@@ -31,7 +47,18 @@ fn main() {
             if builtins.contains(&command_args) {
                 println!("{} is a shell builtin", command_args);
             }else{
-                println!("{}: not found", command_args);
+                // println!("{}: not found", command_args);
+                
+                let matched_path = path.split(":")
+                    .map(Path::new)
+                    .filter(|p| p.is_dir())
+                    .map(|p| p.join(command_args))
+                    .find(|fp| fp.is_file() && is_executable(&fp.display().to_string()));
+                
+                match matched_path {
+                    Some(fp) => println!("{} is {:?}", command_args, fp ),
+                    _ => println!("{}: not found ", command_args),
+                }
             }
         }else{
             println!("{0}: command not found", command.trim());
