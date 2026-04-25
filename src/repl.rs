4c2b47f932b751efbc55027522921ec3a3410f69
Victor (Takunda) Phos <victorphos1@gmail.com>
2026-04-25 15:13:32 +0200

    codecrafters submit [skip ci]

--- src/repl.rs
+++ src/repl.rs
@@ -2,6 +2,15 @@ use std::os::unix::process::CommandExt;
 use std::path::Path;
 use std::process::{Command, Stdio};
 
+use rustyline::completion::Completer;
+use rustyline::error::ReadlineError;
+use rustyline::highlight::Highlighter;
+use rustyline::hint::Hinter;
+use rustyline::validate::Validator;
+use rustyline::{Context, Helper, Editor};
+use rustyline::history::DefaultHistory;
+
+
 use std::io::{self, Write};
 
 use crate::builtins::{BUILTINS, handle_builtins};
@@ -9,17 +18,66 @@ use crate::exec;
 use crate::path;
 use crate::tokenizer;
 
+struct MyCompleter; 
+
+impl Helper for MyCompleter {}
+impl Hinter for MyCompleter {
+    type Hint = String;
+}
+
+impl Highlighter for MyCompleter {}
+impl Validator for MyCompleter {}
+
+impl Completer for MyCompleter {
+    type Candidate = String;
+    
+    fn complete(
+        &self, 
+        line: &str,
+        pos: usize,
+        _: &rustyline::Context
+    ) -> rustyline::Result<(usize, Vec<String>)> {
+        let commands = ["echo", "exit"];
+        
+        let start = line[..pos].rfind(' ').map(|p| p + 1).unwrap_or(0);
+        let prefix = &line[start..pos];
+        
+        let matches: Vec<String> = commands
+            .iter()
+            .filter(|c| c.starts_with(prefix))
+            .map(|c| {
+                let mut s = c.to_string();
+                s.push(' ');
+                s
+            })
+            .collect();
+        Ok((start, matches))
+    }
+}
+
 pub fn start_shell_repl() {
     let path_var = std::env::var("PATH").unwrap();
+    
+    let mut rl: Editor<MyCompleter, DefaultHistory> = Editor::new().unwrap();
+    rl.set_helper(Some(MyCompleter));
 
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
+        // let mut command = String::new();
+        // io::stdin().read_line(&mut command).unwrap();
+        
+        let readline = rl.readline("$ ");
+        
+        
+        let command = match readline {
+            Ok(line) => line.trim().to_string(),
+            Err(ReadlineError::Interrupted | ReadlineError::Eof) => break,
+            Err(_) => continue
+        };
 
-        command = command.trim().to_string();
+        // command = command.trim().to_string();
 
         let (tokens, (redirect_type, redirect_file)) = tokenizer::tokenize(&command);
         
