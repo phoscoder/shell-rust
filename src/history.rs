05b040d73eeed0e4aecfe89644f3d6d5ef458961
Victor Phos <victorphos1@gmail.com>
2026-06-16 11:23:29 +0200

    improv: change history ds to btreemap

--- src/history.rs
+++ src/history.rs
@@ -1,25 +1,26 @@
+use std::collections::{BTreeMap};
+
 
 
 #[derive(Default)]
 pub struct History {
-    history: Vec<String>,
+    history: BTreeMap<usize, String>,
 }
 
 impl History {
     pub fn add(&mut self, cmd: &str) {
-        self.history.push(cmd.to_string());
+        self.history.insert(self.history.len(), cmd.to_string());
     }
 
     pub fn print_history(&self) -> () {
-        for (index, hist) in self.history.iter().enumerate() {
-            println!("{} {}", index + 1, hist);
+        for (k, v) in self.history.iter() {
+            println!("{} {}", k + 1, v);
         }
     }
 
     pub fn print_history_limited(&self, limit:usize) -> () {
-        let last_n = self.history[self.history.len().saturating_sub(limit)..].to_vec();
-        for (index, hist) in last_n.iter().enumerate() {
-            println!("{} {}", index + 1, hist);
+        for (k, v) in self.history.iter().skip(self.history.len().saturating_sub(limit)) {
+            println!("{} {}", k + 1, v);
         }
     }
 }
