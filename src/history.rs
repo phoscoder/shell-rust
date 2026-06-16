533dae7847a9b6e188a4ecbf9f213b0f38860c7e
Victor Phos <victorphos1@gmail.com>
2026-06-16 11:17:42 +0200

    feat: add print_history_limited

--- src/history.rs
+++ src/history.rs
@@ -11,7 +11,14 @@ impl History {
     }
 
     pub fn print_history(&self) -> () {
-        for (index, hist )in self.history.iter().enumerate() {
+        for (index, hist) in self.history.iter().enumerate() {
+            println!("{} {}", index + 1, hist);
+        }
+    }
+
+    pub fn print_history_limited(&self, limit:usize) -> () {
+        let last_n = self.history[self.history.len().saturating_sub(limit)..].to_vec();
+        for (index, hist) in last_n.iter().enumerate() {
             println!("{} {}", index + 1, hist);
         }
     }
